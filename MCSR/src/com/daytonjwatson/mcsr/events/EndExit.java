cec7f0a0db2e08e74c9986eecf40546dc30acc6c
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2026-01-16 20:08:35 -0700

    Fix end portal completion handling

--- MCSR/src/com/daytonjwatson/mcsr/events/EndExit.java
+++ MCSR/src/com/daytonjwatson/mcsr/events/EndExit.java
@@ -1,7 +1,6 @@
 package com.daytonjwatson.mcsr.events;
 
 import org.bukkit.World;
-import org.bukkit.boss.DragonBattle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
@@ -16,21 +15,14 @@ public class EndExit implements Listener {
 	    if (e.getCause() != PlayerTeleportEvent.TeleportCause.END_PORTAL) return;
 
 	    World fromWorld = e.getFrom().getWorld();
-	    World toWorld = (e.getTo() != null) ? e.getTo().getWorld() : null;
-	    if (fromWorld == null || toWorld == null) return;
+	    if (fromWorld == null) return;
 
 	    if (fromWorld.getEnvironment() != World.Environment.THE_END) return;
-	    if (toWorld.getEnvironment() != World.Environment.NORMAL) return;
 
-	    DragonBattle battle = fromWorld.getEnderDragonBattle();
-	    if (battle == null) return;
-
-	    if (battle.hasBeenPreviouslyKilled()) {
-	        String finalTime = TimerManager.stopStopwatch(e.getPlayer());
-	        e.getPlayer().sendMessage("§6Final time: §a" + finalTime);
-	        
-	        e.setTo(Utils.spawnLocation());
-	    }
+	    String finalTime = TimerManager.stopStopwatch(e.getPlayer());
+	    e.getPlayer().sendMessage("§6Final time: §a" + finalTime);
+	    
+	    e.setTo(Utils.spawnLocation());
 	}
 
 }
